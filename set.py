904fcc7e3348aeec8e85011b17fdd10697404e79
dhunganayukta <139676621+dhunganayukta@users.noreply.github.com>
2023-07-16 14:59:31 +0545

    Add files via upload

---
+++ set.py
@@ -0,0 +1,11 @@
+ 
+set1={1, 2, 3}
+set2={ 3, 4, 5}
+set_union= set1.union(set2)
+print(set_union)
+ 
+set_intersection=set1.intersection(set2)
+print(set_intersection)
+
+set_difference=set1.difference(set2)
+print(set_difference)
